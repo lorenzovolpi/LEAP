79c98f6bae0afa6efc387da71f701539905d5f3e
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-06-21 10:34:45 +0200

    datasets used for experiments

---
+++ phd/dataset.py
@@ -0,0 +1,18 @@
+from quapy.data.base import LabelledCollection
+from quapy.data.datasets import fetch_UCIBinaryDataset
+
+from phd.environment import env
+
+TRAIN_VAL_PROP = 0.5
+
+
+class DatasetProvider:
+    @classmethod
+    def _split_train(cls, train: LabelledCollection):
+        return train.split_stratified(TRAIN_VAL_PROP, random_state=0)
+
+    @classmethod
+    def uci_binary(cls, dataset_name, data_home=env["QUAPY_DATA"]):
+        train, U = fetch_UCIBinaryDataset(dataset_name, data_home=data_home).train_test
+        T, V = cls._split_train(train)
+        return T, V, U
