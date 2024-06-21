ac6532df83b005d1647a104b3bff2e2418285186
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2024-06-21 10:33:57 +0200

    experimental protocol for experiments

---
+++ phd/experiments/generators.py
@@ -0,0 +1,88 @@
+import os
+
+import quapy as qp
+from quapy.data.base import LabelledCollection
+from quapy.data.datasets import UCI_BINARY_DATASETS
+from quapy.method._kdey import KDEyML
+from quapy.method.aggregative import ACC
+from sklearn.linear_model import LogisticRegression as LR
+from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.neural_network import MLPClassifier as MLP
+from sklearn.svm import SVC
+
+from phd.dataset import DatasetProvider as DP
+from phd.error import vanilla_acc
+from phd.models.cont_table import (
+    CAPContingencyTable,
+    NaiveCAP,
+    PhD,
+)
+from phd.models.direct import ATC, CAPDirect, DoC
+from phd.utils.commons import get_results_path
+
+
+def gen_classifiers():
+    yield "LR", LR()
+    yield "KNN", KNN(n_neighbors=10)
+    yield "SVM", SVC(probability=True)
+    yield "MLP", MLP(hidden_layer_sizes=(100,) * 5, max_iter=300, random_state=0)
+
+
+def gen_bin_datasets(
+    only_names=False,
+) -> [str, [LabelledCollection, LabelledCollection, LabelledCollection]]:
+    _uci_skip = ["acute.a", "acute.b", "balance.2", "iris.1"]
+    _uci_names = [d for d in UCI_BINARY_DATASETS if d not in _uci_skip]
+    for dn in _uci_names:
+        dval = None if only_names else DP.uci_binary(dn)
+        yield dn, dval
+
+
+def gen_product(gen1, gen2):
+    for v1 in gen1():
+        for v2 in gen2():
+            yield v1, v2
+
+
+### baselines ###
+def gen_CAP_baselines(h, acc_fn, config, with_oracle=False) -> [str, CAPDirect]:
+    yield "ATC", ATC(h, acc_fn, scoring_fn="maxconf")
+    yield "DoC", DoC(h, acc_fn, sample_size=qp.environ["SAMPLE_SIZE"])
+
+
+def gen_CAP_cont_table(h, acc_fn, config) -> [str, CAPContingencyTable]:
+    yield "Naive", NaiveCAP(h, acc_fn)
+    yield "PhD", PhD(h, acc_fn, ACC(LR()), reuse_h=True)
+    yield "PhD-plus", PhD(h, acc_fn, KDEyML(LR()), reuse_h=True)
+
+
+def gen_methods(h, V, config, with_oracle=False):
+    config = "multiclass" if config is None else config
+
+    _, acc_fn = next(gen_acc_measure())
+
+    for name, method in gen_CAP_baselines(h, acc_fn, config, with_oracle):
+        yield name, method, V
+    for name, method in gen_CAP_cont_table(h, acc_fn, config):
+        yield name, method, V
+
+
+def get_method_names(config):
+    mock_h = LR()
+    _, mock_acc_fn = next(gen_acc_measure())
+    return [m for m, _ in gen_CAP_baselines(mock_h, mock_acc_fn, config)] + [
+        m for m, _ in gen_CAP_cont_table(mock_h, mock_acc_fn, config)
+    ]
+
+
+def gen_acc_measure(multiclass=False):
+    yield "vanilla_accuracy", vanilla_acc
+
+
+def any_missing(basedir, cls_name, dataset_name, method_name):
+    for acc_name, _ in gen_acc_measure():
+        if not os.path.exists(
+            get_results_path(basedir, cls_name, acc_name, dataset_name, method_name)
+        ):
+            return True
+    return False
---
+++ phd/experiments/run.py
@@ -0,0 +1,135 @@
+import itertools as IT
+import os
+from traceback import print_exception
+
+import quapy as qp
+from quapy.protocol import UPP
+
+from phd.experiments.generators import (
+    gen_acc_measure,
+    gen_bin_datasets,
+    gen_classifiers,
+    gen_methods,
+    gen_product,
+    get_method_names,
+)
+from phd.experiments.report import TestReport
+from phd.experiments.util import (
+    fit_or_switch,
+    get_logger,
+    get_plain_prev,
+    get_predictions,
+    prevs_from_prot,
+)
+from phd.utils.commons import save_dataset_stats, true_acc
+
+PROBLEM = "binary"
+ORACLE = False
+basedir = PROBLEM + ("-oracle" if ORACLE else "")
+
+NUM_TEST = 1000
+
+if PROBLEM == "binary":
+    qp.environ["SAMPLE_SIZE"] = 100
+    gen_datasets = gen_bin_datasets
+
+log = get_logger()
+
+
+def all_exist_pre_check(basedir, cls_name, dataset_name):
+    method_names = get_method_names(PROBLEM)
+    acc_names = [acc_name for acc_name, _ in gen_acc_measure()]
+
+    all_exist = True
+    for method, acc in IT.product(method_names, acc_names):
+        path = TestReport(
+            basedir, cls_name, acc, dataset_name, None, None, method
+        ).get_path()
+        all_exist = os.path.exists(path)
+        if not all_exist:
+            break
+
+    return all_exist
+
+
+def experiments():
+    log.info("-" * 31 + "  start  " + "-" * 31)
+
+    for (cls_name, h), (dataset_name, (L, V, U)) in gen_product(
+        gen_classifiers, gen_datasets
+    ):
+        if all_exist_pre_check(basedir, cls_name, dataset_name):
+            log.info(
+                f"{cls_name} on dataset={dataset_name}: all results already exist, skipping"
+            )
+            continue
+
+        log.info(f"{cls_name} training on dataset={dataset_name}")
+        h.fit(*L.Xy)
+
+        # test generation protocol
+        test_prot = UPP(
+            U, repeats=NUM_TEST, return_type="labelled_collection", random_state=0
+        )
+
+        # compute some stats of the dataset
+        save_dataset_stats(dataset_name, test_prot, L, V)
+
+        # precompute the actual accuracy values
+
+        true_accs = {}
+        for acc_name, acc_fn in gen_acc_measure(multiclass=True):
+            true_accs[acc_name] = [true_acc(h, acc_fn, Ui) for Ui in test_prot()]
+
+        L_prev = get_plain_prev(L.prevalence())
+        for method_name, method, V in gen_methods(h, V, PROBLEM, ORACLE):
+            V_prev = get_plain_prev(V.prevalence())
+
+            t_train = None
+            for acc_name, acc_fn in gen_acc_measure(multiclass=True):
+                report = TestReport(
+                    basedir,
+                    cls_name,
+                    acc_name,
+                    dataset_name,
+                    L_prev,
+                    V_prev,
+                    method_name,
+                )
+                if os.path.exists(report.get_path()):
+                    log.info(f"{method_name}: {acc_name} exists, skipping")
+                    continue
+
+                try:
+                    method, _t_train = fit_or_switch(
+                        method, V, acc_fn, t_train is not None
+                    )
+                    t_train = t_train if _t_train is None else _t_train
+
+                    test_prevs = prevs_from_prot(test_prot)
+                    estim_accs, t_test_ave = get_predictions(method, test_prot, ORACLE)
+                    report.add_result(
+                        test_prevs, true_accs[acc_name], estim_accs, t_train, t_test_ave
+                    )
+                except Exception as e:
+                    print_exception(e)
+                    log.warning(
+                        f"{method_name}: {acc_name} gave error '{e}' - skipping"
+                    )
+                    continue
+
+                report.save_json(basedir, acc_name)
+
+                log.info(
+                    f"{method_name}: {acc_name} done [t_train:{t_train:.3f}s; t_test_ave:{t_test_ave:.3f}s]"
+                )
+
+    log.info("-" * 32 + "  end  " + "-" * 32)
+
+
+if __name__ == "__main__":
+    try:
+        experiments()
+    except Exception as e:
+        log.error(e)
+        print_exception(e)
