ce466b1e0bef5fc213e92e52913f96f7923419e0
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2025-07-17 01:46:22 +0200

    added qerr experiment and plotting function

--- exp/plot.py
+++ exp/plot.py
@@ -1,7 +1,6 @@
 import itertools as IT
 import math
 import os
-import pdb
 from argparse import ArgumentParser
 
 import matplotlib.pyplot as plt
@@ -9,14 +8,11 @@ import matplotlib.ticker as tkr
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from matplotlib.pyplot import close
 from matplotlib.ticker import LogLocator, MultipleLocator
 
-import exp.config as cfg
 import exp.env as env
 from exp.config import get_acc_names, get_classifier_names, get_dataset_names
 from exp.util import load_results, rename_datasets, rename_methods
-from leap.plot.seaborn import plot_diagonal_grid
 from leap.plot.utils import get_binned_values, save_figure
 
 method_map = {
@@ -52,18 +48,30 @@ def get_selection_datasets():
         return ["poker_hand", "shuttle", "page_block", "phishing"]
 
 
-def plots():
-    def get_palette():
-        base_palette = sns.color_palette("bright", 10)
-        _dict = {
-            "DoC": 0,
-            "LEAP(KDEy-MLP)": 1,
-            "S-LEAP(KDEy-MLP)": 2,
-            "O-LEAP(KDEy-MLP)": 3,
-            "O-LEAP(CC-MLP)": 4,
-            "O-LEAP(oracle)": 8,
-        }
+def get_palette(methods=None):
+    base_palette = sns.color_palette("bright", 10)
+    _dict = {
+        "DoC": 0,
+        "LEAP(ACC)": 6,
+        "LEAP(KDEy-MLP)": 1,
+        "S-LEAP(KDEy-MLP)": 2,
+        "O-LEAP(KDEy-MLP)": 3,
+        "O-LEAP(CC-MLP)": 4,
+        "O-LEAP(oracle)": 8,
+    }
+    if methods is None:
         return {method_map.get(k, k): base_palette[v] for k, v in _dict.items()}
+    else:
+        return {method_map.get(k, k): base_palette[v] for k, v in _dict.items() if method_map.get(k, k) in methods}
+
+
+def plots():
+    # scatter and relplot params
+    hspace = wspace = 0.1
+    xticks = yticks = np.linspace(0, 1, 6, endpoint=True)
+    aspect = 0.8
+    scatter_s = 50
+    alpha = 0.2
 
     all_classifiers = get_classifier_names()
     accs = ["vanilla_accuracy"]
@@ -105,37 +113,65 @@ def plots():
         for cls_name in classifiers:
             res = load_results(acc=acc, classifier=cls_name, filter_methods=methods)
             df = res.loc[(res["dataset"].isin(datasets)) & (res["method"].isin(methods)), :]
-            print(f"Plotting {name} for {cls_name} [{acc}]")
             assert len(df["method"].unique()) == len(methods), (
                 f"Error while generating {name} for {cls_name} [{acc}]: some methods missing!"
             )
             _methods, _df = rename_methods(method_map, methods, df=df)
-            _datasets, _df = rename_datasets(dataset_map, datasets, df=df)
+            _datasets, _df = rename_datasets(dataset_map, datasets, df=_df)
 
-            plot_diagonal_grid(
+            sns.set_context("paper", font_scale=2)
+            plot = sns.relplot(
                 _df,
-                methods_order=_methods,
-                datasets_order=_datasets,
-                basedir=parent_dir,
-                filename=f"grid_{cls_name}_{env.PROBLEM}_{name}",
-                n_cols=4,
-                legend_bbox_to_anchor=(0.95, 0.3),
+                x="true_accs",
+                y="estim_accs",
+                col="dataset",
+                col_order=_datasets,
+                col_wrap=len(_datasets),
+                hue="method",
+                hue_order=_methods,
+                kind="scatter",
+                # scatterplot params
+                alpha=alpha,
+                s=scatter_s,
+                edgecolor=None,
+                facet_kws=dict(
+                    xlim=(0, 1),
+                    ylim=(0, 1),
+                ),
+                aspect=aspect,
                 palette=get_palette(),
-                x_label="True Accuracy",
-                y_label="Estimated Accuracy",
-                aspect=0.8,
-                xtick_vert=True,
-                xticks=np.linspace(0, 1, 6, endpoint=True),
-                yticks=np.linspace(0, 1, 6, endpoint=True),
+            )
+            for ax in plot.axes.flat:
+                ax.axline((0, 0), slope=1, color="black", linestyle="--", linewidth=1)
+                ax.tick_params(axis="x", labelrotation=90)
+                ax.set_xticks(xticks)
+                ax.set_yticks(yticks)
+
+            plot.figure.subplots_adjust(hspace=hspace, wspace=wspace)
+            plot.set_titles("{col_name}")
+
+            plot.legend.set_title(None)
+            sns.move_legend(
+                plot,
+                "center right",
             )
 
+            for lh in plot.legend.legend_handles:
+                lh.set_alpha(1)
+                lh.set_markersize(16)
+
+            plot.set_xlabels("True Accuracy")
+            plot.set_ylabels("Estimated Accuracy")
+
+            save_figure(plot=plot, basedir=parent_dir, filename=f"grid_{cls_name}_{env.PROBLEM}_{name}")
+
+            print(f"Plotted {name} for {cls_name} [{acc}]")
 
 def plot_sample_size():
     from exp.sample_size import get_acc_names as ss_acc_names
     from exp.sample_size import get_classifier_names as ss_classifier_names
     from exp.sample_size import get_dataset_names as ss_dataset_names
 
-    # methods = ["DoC", "LEAP(KDEy-MLP)", "PHD(KDEy-MLP)", "OCE(KDEy-MLP)-SLSQP"]
     methods = ["DoC", "O-LEAP(KDEy-MLP)"]
     classifiers = ss_classifier_names()
     datasets = ss_dataset_names()
@@ -239,10 +275,6 @@ def plot_times():
     methods = [
         "ATC-MC",
         "DoC",
-        # "DS",
-        # "CBPE",
-        # "NN",
-        # "Q-COT",
         "LEAP(KDEy-MLP)",
         "S-LEAP(KDEy-MLP)",
         "O-LEAP(KDEy-MLP)",
@@ -379,7 +411,6 @@ def plot_by_shift():
     n_bins = 20
     classifiers = ["LR"]
     accs = ["vanilla_accuracy"]
-    datasets = get_dataset_names()
     methods = [
         "Naive",
         "ATC-MC",
@@ -424,7 +455,7 @@ def plot_by_shift():
         print(f"Plotted {cls_name} - {acc} - {env.PROBLEM}")
 
 
-def pseudo_label_shift_plot():
+def plot_pseudo_label_shift():
     def get_pseudo_label_shift(df: pd.DataFrame, datasets: list[str]):
         for d in datasets:
             _df = df.loc[df["dataset"] == d, :]
@@ -476,6 +507,85 @@ def pseudo_label_shift_plot():
         print(f"Plotted {cls_name} - {acc} - {env.PROBLEM}")
 
 
+def plot_qerr():
+    from exp.qerr import get_acc_names as qerr_accs
+    from exp.qerr import get_classifier_names as qerr_clssifiers
+    from exp.qerr import get_dataset_names as qerr_datasets
+    from exp.qerr import get_method_names as qerr_methods
+
+    aspect = 0.8
+    xticks = yticks = np.linspace(0, 1, 6, endpoint=True)
+    hspace = wspace = 0.1
+    n_bins = 20
+    classifiers = qerr_clssifiers()
+    accs = qerr_accs()
+    datasets = qerr_datasets()
+    methods = qerr_methods()
+
+    base_dir = os.path.join(env.root_dir, "qerr")
+
+    for acc, cls_name in IT.product(accs, classifiers):
+        df = load_results(base_dir=base_dir, acc=acc, classifier=cls_name, filter_methods=methods)
+        df.loc[:, "q_errs_bin"] = get_binned_values(df, "q_errs", n_bins)
+        _methods, df = rename_methods(method_map, methods, df=df)
+        _datasets, df = rename_datasets(dataset_map, datasets, df=df)
+
+        base_dir = os.path.join(env.root_dir, "plots", "qerr")
+        os.makedirs(base_dir, exist_ok=True)
+
+        sns.set_context("paper", font_scale=2)
+        plot = sns.relplot(
+            df,
+            x="q_errs_bin",
+            y="acc_err",
+            col="dataset",
+            col_order=_datasets,
+            col_wrap=len(_datasets),
+            hue="method",
+            hue_order=_methods,
+            kind="line",
+            # sns.lineplot args
+            estimator="mean",
+            err_style=None,
+            # errorbar="se",
+            # err_style="bars",
+            # err_kws=dict(capsize=2.0, capthick=1.0),
+            linewidth=4,
+            facet_kws=dict(
+                xlim=(0, 1),
+                ylim=(0, 1),
+            ),
+            aspect=aspect,
+            palette=get_palette(),
+        )
+        for ax in plot.axes.flat:
+            ax.tick_params(axis="x", labelrotation=90)
+            ax.set_xticks(xticks)
+            ax.set_yticks(yticks)
+
+        plot.figure.subplots_adjust(hspace=hspace, wspace=wspace)
+
+        # set plot title
+        plot.set_titles("{col_name}")
+
+        # config legend
+        plot.legend.set_title(None)
+        sns.move_legend(
+            plot,
+            "center right",
+        )
+
+        # set axes labels
+        plot.set_xlabels("Quantification Error")
+        plot.set_ylabels("AE")
+
+        plot_dir = os.path.join(env.root_dir, "plots", "qerr")
+        os.makedirs(plot_dir, exist_ok=True)
+
+        save_figure(plot=plot, basedir=plot_dir, filename=f"qerr_{cls_name}_{env.PROBLEM}")
+        print(f"Plotted {cls_name} - {acc} - {env.PROBLEM}")
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
@@ -489,6 +599,7 @@ if __name__ == "__main__":
     )
     parser.add_argument("--shift", action="store_true", help="Plots accuracy error by amount of PPS")
     parser.add_argument("--pseudo", action="store_true", help="Plots accuracy error by amount of pseudo-label shift")
+    parser.add_argument("--qerr", action="store_true", help="Plots accuracy error by amount of quantification error")
     args = parser.parse_args()
 
     if args.problem not in env._valid_problems:
@@ -506,6 +617,8 @@ if __name__ == "__main__":
     elif args.shift:
         plot_by_shift()
     elif args.pseudo:
-        pseudo_label_shift_plot()
+        plot_pseudo_label_shift()
+    elif args.qerr:
+        plot_qerr()
     else:
         parser.print_help()
